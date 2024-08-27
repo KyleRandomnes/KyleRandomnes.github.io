4a58bb64d18b46e76f625953fb1feeb2cb8c59b5
KyleRandomnes <kylejleg@gmail.com>
2024-08-27 16:18:45 +1200

    one last attempt at this

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
         <h1>KyleRandomnes</h1>
         <!-- Links to other Social Media -->
         <p> Contact me: </p>
-        <p><a href="linkTree.php">Link Tree</a></p>
+        <p><a href="linkTree.html">Link Tree</a></p>
 
         <!-- Links to other sites -->
         <p>Navigate my website</p>
-        <p><a href="projects.php">Projects</a></p>
+        <p><a href="projects.html">Projects</a></p>
 
         <!-- Links to references(Styles sheet for example) -->
         <p>References:</p>
-        <p><a href="references.php">References</a></p>
+        <p><a href="references.html">References</a></p>
     </body>
 </html>
