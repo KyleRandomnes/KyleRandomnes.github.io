dde196ff58d08a552c855609d0c972096296ca71
KyleRandomnes <135270676+KyleRandomnes@users.noreply.github.com>
2024-11-05 12:05:10 +1300

    hotkeys

--- functions/hotkeys.js
+++ functions/hotkeys.js
@@ -1,8 +1,21 @@
 
 document.addEventListener('keydown', function(event) {
         // Navigate to the homepage with Alt + H
-        if (event.altKey && event.key === 'k') {
+        if (event.altKey && event.key === 'h') {
             window.location.href = 'index.html';
         }
+        if (event.altKey && event.key === 'u') {
+            window.location.href = 'uniProjects.html';
+        }
+        if (event.altKey && event.key === 'o') {
+            window.location.href = 'ownProjects.html';
+        }
+        if (event.altKey && event.key === 's') {
+            window.location.href = 'selfStudyProjects.html';
+        }
+        if (event.altKey && event.key === 'a') {
+            window.location.href = 'aboutMe.html';
+        }
+
          //More hotkeys to be added
     });
