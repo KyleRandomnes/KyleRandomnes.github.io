67b9dedbfd7c25834a0bf88888a14969aabd12ca
KyleRandomnes <135270676+KyleRandomnes@users.noreply.github.com>
2024-12-02 09:43:42 +1300

    based on prelim tests
    testing in window mode on pc shows that this works kinda of the way I intended which very good to know.

--- functions/hotkeys.js
+++ functions/hotkeys.js
@@ -2,19 +2,19 @@
 document.addEventListener('keydown', function(event) {
         // Navigate to the homepage with Alt + H
         if (event.altKey && event.key === 'h') {
-            window.location.href = 'index.html';
+            window.location.href = '/index.html';
         }
         if (event.altKey && event.key === 'u') {
-            window.location.href = 'uniProjects.html';
+            window.location.href = '/uniProjects.html';
         }
         if (event.altKey && event.key === 'o') {
-            window.location.href = 'ownProjects.html';
+            window.location.href = '/ownProjects.html';
         }
         if (event.altKey && event.key === 's') {
-            window.location.href = 'selfStudyProjects.html';
+            window.location.href = '/selfStudyProjects.html';
         }
         if (event.altKey && event.key === 'a') {
-            window.location.href = 'aboutMe.html';
+            window.location.href = '/aboutMe.html';
         }
         if (event.altKey && event.key ==='b'){
             window.history.back();
